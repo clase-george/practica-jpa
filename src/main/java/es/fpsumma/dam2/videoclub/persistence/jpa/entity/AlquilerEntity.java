d5ce18650dceaad79419161beb599e855f10b21c
georgeeduardtp <georgeeduard.tp@gmail.com>
2025-12-11 16:38:22 +0100

    HEcho el repoitory de director y acabados todos los entity

--- src/main/java/es/fpsumma/dam2/videoclub/persistence/jpa/entity/AlquilerEntity.java
+++ src/main/java/es/fpsumma/dam2/videoclub/persistence/jpa/entity/AlquilerEntity.java
@@ -1,8 +1,11 @@
 package es.fpsumma.dam2.videoclub.persistence.jpa.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 public class AlquilerEntity {
 
@@ -10,11 +13,72 @@ public class AlquilerEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
     private Long clienteId;
 
+    @ManyToOne
+    @JoinColumn(name = "pelicula_id")
     private Long peliculaId;
 
-    private String fecha_alquiler;
-    private String fecha_devolucion;
+    @Column(name = "fecha_alquiler", nullable = false)
+    private String fechaAlquiler;
+
+    @Column(name = "fecha_devolucion")
+    private String fechaDevolucion;
+
+    // ===== Constructores =====//
+    public AlquilerEntity() {
+    }
+
+    public AlquilerEntity(Long id, Long clienteId, Long peliculaId, String fechaAlquiler,
+            String fechaDevolucion) {
+        this.id = id;
+        this.clienteId = clienteId;
+        this.peliculaId = peliculaId;
+        this.fechaAlquiler = fechaAlquiler;
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    // ===== Getters y setters =====//
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getClienteId() {
+        return clienteId;
+    }
+
+    public void setClienteId(Long clienteId) {
+        this.clienteId = clienteId;
+    }
+
+    public Long getPeliculaId() {
+        return peliculaId;
+    }
+
+    public void setPeliculaId(Long peliculaId) {
+        this.peliculaId = peliculaId;
+    }
+
+    public String getFechaAlquiler() {
+        return fechaAlquiler;
+    }
+
+    public void setFechaAlquiler(String fechaAlquiler) {
+        this.fechaAlquiler = fechaAlquiler;
+    }
+
+    public String getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(String fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
 
 }
--- src/main/java/es/fpsumma/dam2/videoclub/persistence/jpa/repository/DirectorRepository.java
+++ src/main/java/es/fpsumma/dam2/videoclub/persistence/jpa/repository/DirectorRepository.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 public interface DirectorRepository extends JpaRepository<DirectorEntity, Long> {
 
     // Ejemplo de método de consulta por nombre
+    @Query("select d from DirectorEntity d where d.nombre = ?1")
     Optional<DirectorEntity> findByNombre(String nombre);
 
     List<DirectorEntity> findByNombreContainingIgnoreCase(String nombre);
@@ -18,5 +19,14 @@ public interface DirectorRepository extends JpaRepository<DirectorEntity, Long> 
     // 2) Mismo efecto pero usando @Query con parámetro nombrado
     @Query("SELECT d FROM DirectorEntity d WHERE d.nombre = :nombre")
     Optional<DirectorEntity> buscarPorNombre(@Param("nombre") String nombre);
+
+    Boolean existsByNombre(String nombre);
+
+    @Query("select d from DirectorEntity d order by d.nombre ASC ")
+    List<DirectorEntity> findAllDirectors();
+
+    // Otra opcion para lo de arriba sin query
+    @Query("select d from DirectorEntity d order by d.nombre")
+    List<DirectorEntity> findAllByOrderByNombreAsc();
 
 }
